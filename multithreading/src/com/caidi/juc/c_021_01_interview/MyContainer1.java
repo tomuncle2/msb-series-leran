46ede9a7c0b7064a62b99db2ae7e61f24057f527
caidi <1289359053@qq.com>
2020-07-05 23:01:02 +0800

    线程基本概念

--- multithreading/src/com/caidi/juc/c_021_01_interview/MyContainer1.java
+++ multithreading/src/com/caidi/juc/c_021_01_interview/MyContainer1.java
@@ -77,15 +77,57 @@ class MyMyContainer1<T> {
 
 	public void put(T t) {
 		System.out.println();
+		synchronized (this) {
+			while (lists.size() == MAX) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			lists.add(t);
+			count++;
+		}
+
 	}
 
 	public  T get() {
-		T t = null;
-		System.out.println("");
-		return t;
+		synchronized (this) {
+			T t = null;
+			while (lists.size() == 0) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			t = lists.removeFirst();
+			count--;
+			return t;
+		}
+
 	}
 
 	public static void main(String[] args) {
+		MyContainer1<String> c = new MyContainer1<>();
+		// 启动消费者线程
+		for(int i=0; i<10; i++) {
+			new Thread(()->{
+				for(int j=0; j<5; j++) System.out.println(c.get());
+			}, "c" + i).start();
+		}
 
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 启动生产者线程
+		for(int i=0; i<2; i++) {
+			new Thread(()->{
+				for(int j=0; j<25; j++) c.put(Thread.currentThread().getName() + " " + j);
+			}, "p" + i).start();
+		}
 	}
 }
